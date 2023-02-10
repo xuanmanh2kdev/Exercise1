6e4854bad1bcc78840f123bf77737750cc546fdc
xuanmanh2kdev <xuanmanh2000k63@gmail.com>
2023-02-10 10:01:02 +0700

    first commit

---
+++ Exercise1.java
@@ -0,0 +1,15 @@
+package exercise;
+/**
+* @author ManhTX1
+*
+*/
+public class Exercise1 {
+/**
+* @param args
+*/
+	public static void main(String[] args) {
+		int number1 = 125;
+		int number2 = 2019;
+		System.out.println("Sum: " + (number1 + number2));
+	}
+}
